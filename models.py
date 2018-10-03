e66d56e797b2eba67c95610900053e0d44563b41
Vladimir Dulenov <originalblack@mail.ru>
2018-10-04 02:10:54 +0300

    update models

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy import func
+from sqlalchemy import func, ForeignKey
+from sqlalchemy.orm import relationship, backref,
 
 
 class User(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(20), unique=True)
     email = db.Column(db.String(120), unique=True)
+    items = relationship('Item', backref='user_info')
 
     def __init__(self, login, email):
         self.login = login
@@ -19,7 +21,7 @@ class User(db.Model):
 
 class Item(db.Model):
     __tablename__ = 'items'
-    item_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     start_date = db.Column(db.DateTime, default=func.now())
     img_url = db.Column(db.String(50))
@@ -27,9 +29,10 @@ class Item(db.Model):
     address = db.Column(db.String(50))
     description = db.Column(db.String(255))
     breed = db.Column(db.String(40))
+    user_id = db.Column(db.Integer, ForeignKey('user.user_id'))
+    #user = relationship('User', backref='items')
 
-    def __init__(self, item_id, title, start_date, img_url, price, address, description, breed):
-        self.item_id = item_id
+    def __init__(self, title, start_date, img_url, price, address, description, breed, user_id):
         self.title = title
         self.start_date = start_date
         self.img_url = img_url
@@ -37,6 +40,7 @@ class Item(db.Model):
         self.address = address
         self.description = description
         self.breed = breed
+        self.user_id = user_id
 
     def __repr__(self):
         return '<id {}>'.format(self.item_id)

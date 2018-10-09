83492d4b41ab95e02e0f93514977974ede9911f7
Vladimir Dulenov <originalblack@mail.ru>
2018-10-09 19:16:20 +0300

    add test paginate, its work

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_paginate import Pagination, get_page_args
 import os
 
 app = Flask(__name__)
@@ -42,6 +43,26 @@ def index_list(breed):
         print(err)
         errors.append("Couldn't connect to database.")
     return render_template('item.html', ad_dicts=items_dict, errors=errors)
+
+users = list(range(100))
+def get_users(offset=0, per_page=10):
+    return users[offset: offset + per_page]
+
+
+@app.route('/test/')
+def test_pagin():
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    total = len(users)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('test.html',
+                           users=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
 
 
 if __name__ == '__main__':

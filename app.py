6fe320d7e2e8bcd3891b2b0daf3586d5a75c6b65
Vladimir Dulenov <originalblack@mail.ru>
2018-10-10 01:43:01 +0300

    done items view by 20items on page and deleted test views

--- app.py
+++ app.py
@@ -23,46 +23,39 @@ def main_index():
     return render_template('index.html', breeds=breed_disct, errors=errors)
 
 
+def get_items_per_page(items_result, offset=0, per_page=10):
+    """
+    :param items_result: result of select db
+    :param offset: from 0 by default
+    :param per_page: show item on page, default = 10
+    :return: slice of select result
+    """
+    return items_result[offset: offset + per_page]
+
+
 @app.route("/items/<breed>/", methods=['GET'])
 def index_list(breed):
-    items_dict = {}
     errors = []
     try:
-        page = int(request.args.get('p'))
-    except:
-        page = 1
-    try:
-        for q in Item.query.filter_by(translate_breed=breed).all():
-            items_dict[q.id] = {'title': q.title}
-            items_dict[q.id].update({'price': q.price})
-            items_dict[q.id].update({'breed': q.breed})
-            items_dict[q.id].update({'address': q.address})
-            items_dict[q.id].update({'desc': q.description})
-            items_dict[q.id].update({'date': q.start_date})
+        result = Item.query.filter_by(translate_breed=breed).all()
+        page, per_page, offset = get_page_args(page_parameter='page',
+                                               per_page_parameter='per_page')
+
+        items = get_items_per_page(result, offset=offset, per_page=per_page)
+        total = len(result)
+
+        pagination = Pagination(page=page, per_page=per_page, total=total,
+                                css_framework='bootstrap4')
+        return render_template('item.html',
+                               items=items,
+                               page=page,
+                               per_page=per_page,
+                               pagination=pagination,
+                               )
     except Exception as err:
         print(err)
-        errors.append("Couldn't connect to database.")
-    return render_template('item.html', ad_dicts=items_dict, errors=errors)
-
-users = list(range(100))
-def get_users(offset=0, per_page=10):
-    return users[offset: offset + per_page]
-
-
-@app.route('/test/')
-def test_pagin():
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    total = len(users)
-    pagination_users = get_users(offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='bootstrap4')
-    return render_template('test.html',
-                           users=pagination_users,
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination,
-                           )
+        errors.append("Couldn't get results.")
+        return render_template('error.html', errors=errors)
 
 
 if __name__ == '__main__':
